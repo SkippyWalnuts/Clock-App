6368f382e8c912d126778541c7061bb563095151
SkippyWalnuts <beccaearly89@gmail.com>
2023-08-18 09:45:59 +0100

    Removed fake data from HTML, added updateTime function JS

--- src/index.js
+++ src/index.js
@@ -1,18 +1,27 @@
+function updateTime() {
+
+//Los-Angeles
+
 let losAngelesElement = document.querySelector("#los-angeles");
 let losAngelesDateElement = losAngelesElement.querySelector(".date");
 let losAngelesTimeElement = losAngelesElement.querySelector(".time");
 let losAngelesTime = moment().tz(`America/Los_Angeles`);
 
-
 losAngelesDateElement.innerHTML = losAngelesTime.format(`dddd Do [of] MMMM YYYY`);
 losAngelesTimeElement.innerHTML = losAngelesTime.format(`h:mm:ss [<small>]A[<small>]`);
 
+
+//Shanghai
 
 let shanghaiElement = document.querySelector("#shanghai");
 let shanghaiDateElement = shanghaiElement.querySelector(".date");
 let shanghaiTimeElement = shanghaiElement.querySelector(".time");
 let shanghaiTime = moment().tz(`Asia/Shanghai`);
 
-
 shanghaiDateElement.innerHTML = shanghaiTime.format(`dddd Do [of] MMMM YYYY`);
 shanghaiTimeElement.innerHTML = shanghaiTime.format(`h:mm:ss [<small>]A[<small>]`);
+
+};
+
+updateTime();
+setInterval(updateTime, 1000);
