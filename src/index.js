bb5af14f82b7e8c0efbb260abff1be89069cd867
SkippyWalnuts <beccaearly89@gmail.com>
2023-08-18 11:28:02 +0100

    Added js updateCity function

--- src/index.js
+++ src/index.js
@@ -25,3 +25,24 @@ shanghaiTimeElement.innerHTML = shanghaiTime.format(`h:mm:ss [<small>]A[<small>]
 
 updateTime();
 setInterval(updateTime, 1000);
+
+
+function updateCity(event) {
+   let cityTimeZone = event.target.value;
+   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+   let cityTime = moment().tz(cityTimeZone);
+   let citiesElement = document.querySelector("#cities");
+   citiesElement.innerHTML = 
+   `<div class="city">
+   <div>
+   <h2>${cityName}</h2>
+   <div class="date">${cityTime.format(`dddd Do [of] MMMM YYYY`)}</div>
+</div>
+   <div class="time">${cityTime.format(`h:mm:ss [<small>]A[<small>]`)}</div>
+
+</div>`
+    
+}
+
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
